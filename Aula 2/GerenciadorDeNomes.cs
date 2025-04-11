83a170ae2e6e59d16f1283ca32dcd5d1195c3af4
Rayaaany <rayanyf8@gmail.com>
2025-04-10 21:57:03 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 2/GerenciadorDeNomes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_2
+{
+    public class GerenciadorDeNomes
+    {
+        private readonly string _caminhoArquivo;
+
+        public GerenciadorDeNomes(string caminhoArquivo)
+        {
+            _caminhoArquivo = caminhoArquivo;
+        }
+
+        public void AdicionarNome()
+        {
+            Console.Write("Digite um nome: ");
+            string nome = Console.ReadLine();
+            string dataHora = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+            string linha = $"{dataHora} - {nome}";
+            File.AppendAllText(_caminhoArquivo, linha + Environment.NewLine);
+
+            Console.WriteLine("Nome salvo com sucesso!");
+        }
+
+        public void ListarNomes()
+        {
+            Console.WriteLine("\n--- Lista de nomes persistidos ---");
+
+            if (File.Exists(_caminhoArquivo))
+            {
+                string[] linhas = File.ReadAllLines(_caminhoArquivo);
+
+                foreach (string linha in linhas)
+                {
+                    Console.WriteLine("- " + linha);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum nome foi salvo ainda.");
+            }
+        }
+    }
+}
---
+++ Aula 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using Aula_2;
+
+class Program
+{
+    static void Main()
+    {
+        string caminhoArquivo = "dados.txt";
+        GerenciadorDeNomes gerenciador = new GerenciadorDeNomes(caminhoArquivo);
+
+        Console.WriteLine("=== EXEMPLO DE PERSISTÊNCIA DE DADOS ===");
+
+        while (true)
+        {
+            Console.WriteLine("\n1 - Adicionar nome");
+            Console.WriteLine("2 - Listar nomes");
+            Console.WriteLine("0 - Sair");
+            Console.Write("Escolha uma opção: ");
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    gerenciador.AdicionarNome();
+                    break;
+                case "2":
+                    gerenciador.ListarNomes();
+                    break;
+                case "0":
+                    Console.WriteLine("Encerrando o programa.");
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+        }
+    }
+}
+
